bbe111adbafae8e65f9aa9a1c47e2e317c160d06
JoeJRW <1229858072@qq.com>
2020-07-07 10:35:37 +0800

    push master

---
+++ app/src/main/java/com/whu/dadatraffic/Activity/Splash.java
@@ -0,0 +1,32 @@
+package com.whu.dadatraffic.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.whu.dadatraffic.MainActivity;
+import com.whu.dadatraffic.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Timer timer = new Timer();
+        timer.schedule(task, 2000);
+    }
+
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            startActivity(new Intent(Splash.this, MainActivity.class));
+        }
+    };
+
+}
