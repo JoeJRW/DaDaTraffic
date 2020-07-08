05c3b50f4f4c592324ff9114ef122ac2a2bbd589
Lagueen <543776833@qq.com>
2020-07-08 12:39:53 +0800

    搭好投诉页面，实现形成页面、支付页面、评价页面到投诉页面的跳转

--- app/src/main/java/com/whu/dadatraffic/Activity/OrderendActivity.java
+++ app/src/main/java/com/whu/dadatraffic/Activity/OrderendActivity.java
@@ -2,9 +2,17 @@ package com.whu.dadatraffic.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.whu.dadatraffic.R;
+import com.whu.dadatraffic.MainActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class OrderendActivity extends AppCompatActivity {
 
@@ -12,5 +20,32 @@ public class OrderendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orderend);
+
+        //点击投诉按钮跳转投诉界面
+        Button bt1=findViewById(R.id.end_tousu_button);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(OrderendActivity.this,TousuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //点击提交按钮，显示提交成功
+        Button bt2=findViewById(R.id.submitcomment);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(OrderendActivity.this,"提交成功",Toast.LENGTH_SHORT).show();
+                Timer timer = new Timer();
+                timer.schedule(task, 3000);
+            }
+            TimerTask task=new TimerTask() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(OrderendActivity.this, MainActivity.class));
+                }
+            };
+        });
     }
 }
