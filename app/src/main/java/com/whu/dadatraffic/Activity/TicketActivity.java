4545d1a91077463c95ad8586eb68206623a387de
1378212186@qq.com <1378212186@qq.com>
2020-07-09 22:05:24 +0800

    Revert "添加积分商城中商品相关基类与控制类，优惠券相关基类与控制类"
    This reverts commit fbe3e4efda0222ec0fa6c3d02e3aea7fc609e8cb.

--- app/src/main/java/com/whu/dadatraffic/Activity/TicketActivity.java
+++ app/src/main/java/com/whu/dadatraffic/Activity/TicketActivity.java
@@ -1,114 +1,26 @@
 package com.whu.dadatraffic.Activity;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.whu.dadatraffic.Base.ViewHolder;
-import com.whu.dadatraffic.R;
-import com.whu.dadatraffic.Service.TicketService;
-
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import android.widget.ImageButton;
+import com.whu.dadatraffic.R;
 
 public class TicketActivity extends AppCompatActivity {
 
-    TicketService ticketService = new TicketService();
-    String giveTime = null;
-    String endTime = "2021年8月1日";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket);
-
-        setCustomActionBar();
-
-        Date date = new Date(System.currentTimeMillis());
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日");
-        giveTime = simpleDateFormat.format(date);
-        ticketService.AddTicket("38元打车券","8折",R.drawable.icon_38,giveTime,endTime,true);
-        //ticketService.AddTicket("8折优惠券","8折",R.drawable.icon_card);
-        //初始化ListView控件
-        ListView listView=findViewById(R.id.tlv);
-        //创建一个Adapter的实例
-        TicketAdapter newAdapter=new TicketAdapter();
-        //设置Adapter
-        listView.setAdapter(newAdapter);
-    }
-
-    class TicketAdapter extends BaseAdapter {
-
-        @Override
-        public int getCount(){       //得到item的总数
-
-            return ticketService.Count();    //返回ListView Item条目代表的对象
-        }
-
-        @Override
-        public Object getItem(int position)
-        {
-            return ticketService.GetTitle(position); //返回item的数据对象
-        }
-        @Override
-        public long getItemId(int position)
-        {
-            return position;         //返回item的id
-        }
-
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent){//获取item中的View视图,使用convertview增加系统效率
-            ViewHolder holder;
-            if(convertView==null)//convertview为空则绑定各控件
-            {
-                convertView=View.inflate(TicketActivity.this, R.layout.activity_items, null);
-                holder=new ViewHolder();
-                holder.title=convertView.findViewById(R.id.title);
-                holder.price=convertView.findViewById(R.id.price);
-                holder.image=convertView.findViewById(R.id.item);
-                convertView.setTag(holder);
+        //设置返回按钮
+        ImageButton backButton = (ImageButton) findViewById(R.id.TicketBackBtn);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //实现返回
+                finish();
             }
-            else//非空，则复用convertview
-            {
-                holder=(ViewHolder)convertView.getTag();
-            }
-            //设置该View中各项值
-            ticketService.GetTitle(position);
-            holder.title.setText(ticketService.GetTitle(position));
-            holder.price.setText(ticketService.GetDiscount(position));
-            holder.image.setImageResource(ticketService.GetIcon(position));
-            return convertView;
-        }
+        });
     }
-
-    //显示home按钮与标题
-    private void setCustomActionBar() {
-        //1.获取 actionbar 对象
-        ActionBar actionBar = this.getSupportActionBar();
-        //2.显示home键
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        //3.设置标题
-        actionBar.setTitle("优惠券");
-    }
-
-    //返回父活动
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == android.R.id.home)
-        {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
--- app/src/main/java/com/whu/dadatraffic/Activity/WalletActivity.java
+++ app/src/main/java/com/whu/dadatraffic/Activity/WalletActivity.java
@@ -1,17 +1,18 @@
 package com.whu.dadatraffic.Activity;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-
-import com.whu.dadatraffic.R;
 import android.widget.Button;
-import android.widget.RelativeLayout;
+import android.widget.ImageButton;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.whu.dadatraffic.MainActivity;
+import com.whu.dadatraffic.R;
 
 public class WalletActivity extends AppCompatActivity {
 
@@ -20,6 +21,7 @@ public class WalletActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
         Button ticketBtn = (Button) findViewById(R.id.ticketBtn);//优惠券按钮
+        ImageButton backBtn = (ImageButton) findViewById(R.id.BackBtn) ;
         ticketBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,58 +34,12 @@ public class WalletActivity extends AppCompatActivity {
                 startActivity(intentToTicket);
             }
         });
-
-        RelativeLayout scoreLayout = findViewById(R.id.score_layout);//积分文本
-        scoreLayout.setOnClickListener(new View.OnClickListener() {
+        backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //跳转到积分商城界面
-                //定义跳转对象
-                Intent intentToWallet = new Intent();
-                //设置跳转的起始界面和目的界面
-                intentToWallet.setClass(WalletActivity.this, MarketActivity.class);
-                //启动跳转
-                startActivity(intentToWallet);
+                //实现返回
+                finish();
             }
         });
-
-        RelativeLayout moneyLayout = findViewById(R.id.money_layout);//余额文本
-        moneyLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //跳转到充值界面
-                //定义跳转对象
-                Intent intentToWallet = new Intent();
-                //设置跳转的起始界面和目的界面
-                intentToWallet.setClass(WalletActivity.this, MarketActivity.class);
-                //启动跳转
-                startActivity(intentToWallet);
-            }
-        });
-
-        setCustomActionBar();
     }
-
-//显示home按钮与标题
-    private void setCustomActionBar() {
-        //1.获取 actionbar 对象
-        ActionBar actionBar = this.getSupportActionBar();
-        //2.显示home键
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        //3.设置标题
-        actionBar.setTitle("我的钱包");
-    }
-
-
-    //返回父活动
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == android.R.id.home)
-        {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
