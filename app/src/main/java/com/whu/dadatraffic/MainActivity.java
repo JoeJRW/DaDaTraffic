264bb94c1be7fa2126fd6638dbf4762850c0c630
JoeJRW <1229858072@qq.com>
2020-07-09 20:07:54 +0800

    完成地图定位

--- app/src/main/java/com/whu/dadatraffic/MainActivity.java
+++ app/src/main/java/com/whu/dadatraffic/MainActivity.java
@@ -25,6 +25,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 
+import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
@@ -50,8 +51,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private EditText et_departure;
     private EditText et_destination;
-    private TextView tv_travel;
     private Button btn_travel;
+
+    private LocationClient locationClient;
+    private MapView mapView;
+    private BaiduMap mapLayer;
+    private boolean isFirstLoc = true;
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,10 +71,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         drawerLayout = findViewById(R.id.drawer_layout);
         et_departure = (EditText) findViewById(R.id.et_departure);
         et_destination = (EditText) findViewById(R.id.et_destination);
-        tv_travel = (TextView) findViewById(R.id.tv_travel);
         btn_travel = (Button) findViewById(R.id.btn_travel);
         btn_travel.setOnClickListener(this);
 
+        initLocation();
         initActionBar();
 //--------------侧拉框中多个界面的跳转----------------------------------
         RelativeLayout userLayout = findViewById(R.id.user_layout);
@@ -119,7 +126,95 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
     }
-//----------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
+    /**
+     * 初始化定位参数配置
+     */
+
+    private void initLocation() {
+        mapView=findViewById(R.id.bmapView);
+        mapView.setVisibility(View.INVISIBLE);
+        mapLayer=mapView.getMap();
+        mapLayer.setMyLocationEnabled(true);
+        //定位服务的客户端。宿主程序在客户端声明此类，并调用，目前只支持在主线程中启动
+        locationClient = new LocationClient(this);
+        //声明LocationClient类实例并配置定位参数
+        LocationClientOption locationOption = new LocationClientOption();
+        MyLocationListener myLocationListener = new MyLocationListener();
+        //注册监听函数
+        locationClient.registerLocationListener(myLocationListener);
+        //可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        locationOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        //可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
+        locationOption.setCoorType("bd09ll");
+        //可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
+        locationOption.setScanSpan(1000);
+        //可选，设置是否需要地址信息，默认不需要
+        locationOption.setIsNeedAddress(true);
+        //可选，设置是否需要地址描述
+        locationOption.setIsNeedLocationDescribe(true);
+        //可选，设置是否需要设备方向结果
+        locationOption.setNeedDeviceDirect(false);
+        //可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        locationOption.setLocationNotify(true);
+        //可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        locationOption.setIgnoreKillProcess(true);
+        //可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        locationOption.setIsNeedLocationDescribe(true);
+        //可选，默认false，设置是否收集CRASH信息，默认收集
+        locationOption.SetIgnoreCacheException(false);
+        //可选，默认false，设置是否开启Gps定位
+        locationOption.setOpenGps(true);
+        //可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
+        locationOption.setIsNeedAltitude(false);
+        //设置打开自动回调位置模式，该开关打开后，期间只要定位SDK检测到位置变化就会主动回调给开发者，该模式下开发者无需再关心定位间隔是多少，定位SDK本身发现位置变化就会及时回调给开发者
+        locationOption.setOpenAutoNotifyMode();
+        //设置打开自动回调位置模式，该开关打开后，期间只要定位SDK检测到位置变化就会主动回调给开发者
+        locationOption.setOpenAutoNotifyMode(3000,1, LocationClientOption.LOC_SENSITIVITY_HIGHT);
+        //需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
+        locationClient.setLocOption(locationOption);
+        //开始定位
+        locationClient.start();
+    }
+    /**
+     * 实现定位回调
+     */
+    public class MyLocationListener implements BDLocationListener {
+        //在接收到定位消息时触发
+        @Override
+        public void onReceiveLocation(BDLocation location){
+            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+            //以下只列举部分获取经纬度相关（常用）的结果信息
+            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+            if(location==null||mapView==null){
+                return;
+            }
+            //获取纬度信息
+            latitude = location.getLatitude();
+            //获取经度信息
+            longitude = location.getLongitude();
+            //String position = String.format("当前位置: %s|%s|%s|%s|%s|%s|%s",
+            //        location.getProvince(),location.getCity(),
+            //        location.getDistrict(),location.getStreet(),
+            //        location.getStreetNumber(),location.getAddrStr(),
+            //        location.getTime());
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    //此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(100).latitude(latitude).longitude(longitude)
+                    .build();
+            mapLayer.setMyLocationData(locData);//给地图图层设置定位地点
+            if(isFirstLoc) {//首次定位
+                isFirstLoc=false;
+                LatLng ll = new LatLng(latitude,longitude);//创建一个经纬度对象
+                MapStatusUpdate update = MapStatusUpdateFactory.newLatLngZoom(ll,14);
+                mapLayer.animateMapStatus(update);//设置地图图层的地理位置与缩放比例
+                mapView.setVisibility(View.VISIBLE);//定位到当前城市时再显示图层
+            }
+
+        }
+    }
+//--------------------------------------------------------------------------------------------------
     private void initActionBar() {
         //1.获取 actionbar 对象
         ActionBar actionBar = getSupportActionBar();
@@ -176,6 +271,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             btn_travel.setTextColor(getResources().getColor(R.color.dark_grey));
             btn_travel.setEnabled(false);
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        locationClient.stop();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
     }
 
 }
